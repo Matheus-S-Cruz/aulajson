aeac4233b559144db2d10a6d876e7468ea1373aa
Matheus Cruz <127210839+Matheus-S-Cruz@users.noreply.github.com>
2023-09-22 11:44:10 -0300

    continuação

---
+++ logado/home_script.js
@@ -0,0 +1,7 @@
+let usuario = JSON.parse(localStorage.getItem("logado"));
+document.getElementById("titulo").innerHTML = "Bem vindo, "+usuario.login+"!";
+
+
+function deslogar(){
+    window.location.href = "../index.html"
+}
