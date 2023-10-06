b10eeaa947b9f82d3a4c940dfba196e6863c7881
Matheus Cruz <127210839+Matheus-S-Cruz@users.noreply.github.com>
2023-10-06 12:34:39 -0300

    Add files via upload

--- logado/home_script.js
+++ logado/home_script.js
@@ -5,3 +5,25 @@ document.getElementById("titulo").innerHTML = "Bem vindo, "+usuario.login+"!";
 function deslogar(){
     window.location.href = "../index.html"
 }
+function voltar(){
+    window.location.href = "../logado/home.html"
+}
+
+function montanhas(){
+    window.location.href = "../biomas/montanhas.html"
+}
+function savanas(){
+    window.location.href = "../biomas/savanas.html"
+}
+function praias(){
+    window.location.href = "../biomas/praias.html"
+}
+function florestas(){
+    window.location.href = "../biomas/florestas.html"
+}
+function cavernas(){
+    window.location.href = "../biomas/cavernas.html"
+}
+function ilhas(){
+    window.location.href = "../biomas/ilhas.html"
+}
